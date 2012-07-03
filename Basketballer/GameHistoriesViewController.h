6980f28ed7a1d90c4ddcae29209ac48f7c35e5ec
mao yu <maoyu417@163.com>
2012-07-03 16:49:36 +0800

    创建新项目
    1 支持ARC 2支持Cocoa Data

---
+++ Basketballer/GameHistoriesViewController.h
@@ -0,0 +1,13 @@
+//
+//  GameHistoriesViewController.h
+//  Basketballer
+//
+//  Created by maoyu on 12-7-3.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameHistoriesViewController : UITableViewController
+
+@end
