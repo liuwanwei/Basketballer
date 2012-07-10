fe73edc660d83eebec4b07c1228ff21a950aba3a
mao yu <maoyu417@163.com>
2012-07-10 14:24:44 +0800

    项目新文件

---
+++ Basketballer/difine.m
@@ -0,0 +1,13 @@
+//
+//  difine.m
+//  Basketballer
+//
+//  Created by lixiaoyu on 12-7-4.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "difine.h"
+
+@implementation difine
+
+@end
