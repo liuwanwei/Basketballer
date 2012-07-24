b5b1857d8bbfb18ed3624e7ce43c937770c5c9e5
mao yu <maoyu417@163.com>
2012-07-24 08:23:53 +0800

    添加新文件，引起工程文件修改

---
+++ Basketballer/LocationManager.h
@@ -0,0 +1,13 @@
+//
+//  LocationManager.h
+//  Basketballer
+//
+//  Created by lixiaoyu on 12-7-23.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LocationManager : NSObject
+
+@end
---
+++ Basketballer/LocationManager.m
@@ -0,0 +1,13 @@
+//
+//  LocationManager.m
+//  Basketballer
+//
+//  Created by lixiaoyu on 12-7-23.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "LocationManager.h"
+
+@implementation LocationManager
+
+@end
