960151edf903d066b7d29edc847c3335b15cacf7
mao yu <maoyu417@163.com>
2012-07-24 08:24:07 +0800

    项目新文件

--- Basketballer/LocationManager.h
+++ Basketballer/LocationManager.h
@@ -2,12 +2,18 @@
 //  LocationManager.h
 //  Basketballer
 //
-//  Created by lixiaoyu on 12-7-23.
+//  Created by maoyu on 12-7-23.
 //  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface LocationManager : NSObject
+@interface LocationManager : NSObject <CLLocationManagerDelegate>
+
++ (LocationManager *)defaultManager;
+
+- (void)startStandardLocationServcie;
+- (void)stopStandardLocationService;
 
 @end
--- Basketballer/LocationManager.m
+++ Basketballer/LocationManager.m
@@ -2,12 +2,65 @@
 //  LocationManager.m
 //  Basketballer
 //
-//  Created by lixiaoyu on 12-7-23.
+//  Created by maoyu on 12-7-23.
 //  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
 //
 
 #import "LocationManager.h"
 
+static LocationManager * sLocationManager;
+
+@interface LocationManager() {
+    CLLocationManager * _locationManager;
+}
+@end
+
 @implementation LocationManager
+
++ (LocationManager *)defaultManager{
+    if (nil == sLocationManager) {
+        sLocationManager = [[LocationManager alloc] init];
+    }
+    
+    return sLocationManager;
+}
+
+- (id)init{
+    if (self = [super init]) {
+        _locationManager = [[CLLocationManager alloc] init];
+        _locationManager.delegate = self;
+        _locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters;
+        _locationManager.distanceFilter = 10;
+    }
+    
+    return self;
+}
+
+- (void)startStandardLocationServcie{
+    if ([CLLocationManager locationServicesEnabled]) {
+         [_locationManager startUpdatingLocation];
+    }
+}
+
+- (void)stopStandardLocationService{
+    [_locationManager stopUpdatingLocation];
+}
+
+#pragma mark - CLLocationManagerDelegate
+- (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation{
+    NSDate * date = newLocation.timestamp;
+    NSTimeInterval howRecent = [date timeIntervalSinceNow];
+    if (abs(howRecent) < 5) {
+        [self stopStandardLocationService];
+    }
+}
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error{
+    NSString * message = [error description];
+    UIAlertView * alert = [[UIAlertView alloc] initWithTitle:@"定位" message:message delegate:self cancelButtonTitle:@"确定" otherButtonTitles:nil, nil];
+    [alert show];
+    
+    [self stopStandardLocationService];
+}
 
 @end
